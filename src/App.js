ee9da01d5e40c8455d1291e94dcb5d7748a80cc2
Rafiul Islam <www.rafiulislam1998@gmail.com>
2024-03-20 01:06:04 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import app from './Firebase/firebase.init';
+import { useState } from 'react';
+
+// step -1 get the auth and call the app
+const  auth = getAuth(app)
 
 function App() {
+  const [user ,setUser] = useState({})
+//  step -2 Call the provider and set it
+  const googleProvider = new GoogleAuthProvider()
+  const githubProvider =new GithubAuthProvider()
+
+  //  google login 
+  const handleGoogleLogin = ()=>{
+    signInWithPopup(auth ,googleProvider)
+    .then(result => {
+
+      const user = result.user
+      setUser(user)
+      // console.log(user)
+    })
+    .catch(error =>{
+      setUser({})
+      console.log('error : ' ,error)
+    })
+  }
+  // git hub login 
+  const handelGitHubLogin = () =>{
+    signInWithPopup(auth , githubProvider)
+    .then(result => {
+      const user = result.user
+      setUser(user)
+      console.log(user)
+    })
+    .catch(error =>{
+      setUser({})
+      console.log('error : ' ,error)
+    })
+  }
+  // log out Here 
+  const handleLogOut=()=>{
+    signOut(auth).then(() => {
+      setUser({})
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+       {user.uid ? 
+            <div className='button-style'>
+              <button onClick={() => handleLogOut() }>LogOut</button>
+            </div>
+            :
+            <div className='button-style'>
+            <button onClick={ () =>handleGoogleLogin()}>Google Login</button>
+            <button onClick={ () => handelGitHubLogin()}>Github Login</button>
+            </div>
+        }
+       <div>
+        { user.uid && <div>
+            <h3> Email : {user.email}</h3>
+            <h4> Name : {user.displayName}</h4>
+            <img src={user.photoURL} alt="" />
+        </div> }
+       </div>
     </div>
   );
 }
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6twWUh4yxG7DUcc83BiKMX5ETJxXKKHE",
+  authDomain: "first-firebase-authontic-e521c.firebaseapp.com",
+  projectId: "first-firebase-authontic-e521c",
+  storageBucket: "first-firebase-authontic-e521c.appspot.com",
+  messagingSenderId: "923068608035",
+  appId: "1:923068608035:web:86a782c79ad047b6140b96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
